51ace9fff2458c9ebef3b69df78220b16b1072ed
Austin Davis <davisaustin@berkeley.edu>
2018-10-10 14:08:51 -0700

    added constructors

--- app/src/main/java/com/example/davis/weather/MainActivity.java
+++ app/src/main/java/com/example/davis/weather/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.davis.weather;
 
 import android.support.constraint.Placeholder;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,14 +15,25 @@ import com.example.davis.weatherapp.R;
 //test
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 
     public static class TodayFragment extends Fragment {
 
+        public TodayFragment() {
+
+        }
+        public static TodayFragment newInstance() {
+            return new TodayFragment();
+        }
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -32,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
 
     public static class ForecastFragment extends Fragment {
 
+        public ForecastFragment() {
+
+        }
+        public static ForecastFragment newInstance() {
+            return new ForecastFragment();
+        }
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
