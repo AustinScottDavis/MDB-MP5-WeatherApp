0b984a8b19f1ddfeeab7ff4550ecdf010ad829cc
michelle54725 <michelle54725@gmail.com>
2018-10-12 17:04:54 -0700

    merged location and json parsing

--- app/src/main/java/com/example/davis/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/davis/weatherapp/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.davis.weatherapp;
 
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonStreamParser;
-
-import org.json.JSONObject;
-import org.w3c.dom.Text;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -29,30 +43,39 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 //test
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
     private FragmentManager fm;
     final private String tag = "fragTag";
     private static final String sURL = "https://api.darksky.net/forecast/295a15b47e1a3e4649c5f43bfa41a17e/37.8267,-122.4233";
     public String json;
+
+    // Location-related declarations
+    private static String mLocationString;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+    private LocationManager mLocationManager;
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 6 * 600000; // 60 min
+    private long FASTEST_INTERVAL = 300000; // 5min
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fm = getSupportFragmentManager();
-        try {
-            json = new JsonTask().execute(sURL).get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Utils.parseJSON(json);
+
+        checkLocation();
+        startLocationUpdates(); // sets Utils.location
+
+        setUp();
 
         TodayFragment todayFrag = new TodayFragment();
         fm.beginTransaction().add(R.id.fragment_container, todayFrag).commit();
-        //todayFrag.setText();
 
         findViewById(R.id.toggle_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,14 +95,138 @@ public class MainActivity extends AppCompatActivity {
                 fm.popBackStack();
             }
         });
-
     }
 
-    public static class TodayFragment extends Fragment {
+    // Location stuff (from implementing GoogleApiClient)
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        startLocationUpdates();
+    }
 
-        public TodayFragment() {
+    @Override
+    public void onConnectionSuspended(int i) { }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) { }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+
+//        TextView mLocationView = findViewById(R.id.locationView);
+//        mLocationView.setText(location.getLatitude()) + ", " + String.valueOf(location.getLongitude());
+        Utils.location = String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude());
+
+        // You can now create a LatLng Object for use with maps
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    // Location helpers
+    protected void startLocationUpdates() {
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // Check if permission granted. If not, request it
+        checkLocationPermission();
+
+        getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());
+    }
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+            return false;
+        } else {
+            return true;
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                    if (ContextCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+                        startLocationUpdates();
+                    }
+
+                } else {
+                    // permission denied
+                    Toast.makeText(this,"Permission denied! App cannot function as normal.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                    }
+                });
+        dialog.show();
+    }
+
+    private boolean isLocationEnabled() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    // Fragment stuff
+    public static class TodayFragment extends Fragment {
+        public TodayFragment() { }
 
         public static TodayFragment newInstance() {
             return new TodayFragment();
@@ -93,6 +240,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onActivityCreated(Bundle savedInstanceState) {
             super.onActivityCreated(savedInstanceState);
+            TextView loc = getView().findViewById(R.id.locationView);
+            loc.setText(Utils.location);
 
             TextView desc = getView().findViewById(R.id.descView);
             desc.setText(Utils.description);
@@ -101,9 +250,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static class ForecastFragment extends Fragment {
 
-        public ForecastFragment() {
-
-        }
+        public ForecastFragment() { }
 
         public static ForecastFragment newInstance() {
             return new ForecastFragment();
@@ -111,18 +258,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-
             return inflater.inflate(R.layout.forecast, container, false);
         }
 
     }
 
+    // JSON stuff
     private class JsonTask extends AsyncTask<String, String, String> {
 
         protected String doInBackground(String... params) {
-
-
             HttpURLConnection connection = null;
             BufferedReader reader = null;
 
@@ -171,5 +315,14 @@ public class MainActivity extends AppCompatActivity {
             super.onPostExecute(result);
 
         }
+    }
+
+    private void setUp() {
+        try {
+            json = new JsonTask().execute(sURL).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Utils.parseJSON(json);
     }
 }
